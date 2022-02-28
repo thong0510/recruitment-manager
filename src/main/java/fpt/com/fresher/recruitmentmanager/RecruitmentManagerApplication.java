5f0c0e89bce12c33b62dfb49f28cb37d65c312a3
thong0510 <thong0510@gmail.com>
2022-02-28 19:50:24 +0700

    create project

---
+++ src/main/java/fpt/com/fresher/recruitmentmanager/RecruitmentManagerApplication.java
@@ -0,0 +1,13 @@
+package fpt.com.fresher.recruitmentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitmentManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fpt/com/fresher/recruitmentmanager/RecruitmentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.com.fresher.recruitmentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
